8db135bd45604b9f2e5649dddd5480db593e6708
José García <gran.pepe@gmail.com>
2022-08-24 16:50:43 +0200

    añadidas flecha arriba/abajo y q a keycode

--- so_long/key_pressed.c
+++ so_long/key_pressed.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   key_pressed.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pmoreno- <pmoreno-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: josgarci <josgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 20:18:32 by pmoreno-          #+#    #+#             */
-/*   Updated: 2022/04/29 19:47:21 by pmoreno-         ###   ########.fr       */
+/*   Updated: 2022/08/24 16:49:03 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ void	steps(t_data *data)
 
 int	key_hook(int keycode, t_data *data)
 {
-	if (keycode == 53 || keycode == 49)
+	if (keycode == 53 || keycode == 49 || keycode == 12)
 		exit_game(data);
-	if (keycode == 1)
+	if (keycode == 1 || keycode == 125)
 		go_down(data);
-	if (keycode == 13)
+	if (keycode == 13 || keycode == 126)
 		go_up(data);
 	if (keycode == 2)
 		go_right(data);
